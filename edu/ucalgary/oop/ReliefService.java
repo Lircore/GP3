6d4b5dec2c42de82ff426aebfba0742f93ba4ccc
Lircore <150568101+Lircore@users.noreply.github.com>
2025-02-27 14:43:52 -0700

    Update ReliefService.java
    Added comments, restructures code a little bit, reforms date function to the one used in DisasterVictim
    
    Passes all tests

--- edu/ucalgary/oop/ReliefService.java
+++ edu/ucalgary/oop/ReliefService.java
@@ -1,12 +1,15 @@
 package edu.ucalgary.oop;
 
-// ReliefService Class Implementation
+import java.util.regex.Pattern;
+
 public class ReliefService {
     private Inquirer inquirer;
     private DisasterVictim missingPerson;
     private String dateOfInquiry;
     private String infoProvided;
     private Location lastKnownLocation;
+    private static final Pattern DATE_PATTERN = Pattern.compile("\\d{4}-\\d{2}-\\d{2}");
+
 
     public ReliefService(Inquirer inquirer, DisasterVictim missingPerson, String dateOfInquiry, String infoProvided,
             Location lastKnownLocation) {
@@ -20,57 +23,47 @@ public class ReliefService {
         this.lastKnownLocation = lastKnownLocation;
     }
 
-    public Inquirer getInquirer() {
-        return inquirer;
-    }
+    // Inquirer Functions
+    public Inquirer getInquirer() { return inquirer; }
+    public void setInquirer(Inquirer inquirer) { this.inquirer = inquirer; }
 
-    public void setInquirer(Inquirer inquirer) {
-        this.inquirer = inquirer;
-    }
-
-    public DisasterVictim getMissingPerson() {
-        return missingPerson;
-    }
-
+    // missing persons functions
+    public DisasterVictim getMissingPerson() { return missingPerson; }
     public void setMissingPerson(DisasterVictim missingPerson) {
         this.missingPerson = missingPerson;
     }
 
-    public String getDateOfInquiry() {
-        return dateOfInquiry;
-    }
-
+    // Date Functions
+    public String getDateOfInquiry() { return dateOfInquiry; }
+    
     public void setDateOfInquiry(String dateOfInquiry) {
         if (!isValidDateFormat(dateOfInquiry)) {
             throw new IllegalArgumentException("Invalid date format. Use YYYY-MM-DD.");
         }
         this.dateOfInquiry = dateOfInquiry;
     }
-
-    public String getInfoProvided() {
-        return infoProvided;
+    private boolean isValidDateFormat(String date) {
+        return DATE_PATTERN.matcher(date).matches();
     }
 
+    // Info functions
+    public String getInfoProvided() { return infoProvided; }
     public void setInfoProvided(String infoProvided) {
         this.infoProvided = infoProvided;
     }
 
-    public Location getLastKnownLocation() {
-        return lastKnownLocation;
-    }
-
+    // Location Functions
+    public Location getLastKnownLocation() { return lastKnownLocation; }
     public void setLastKnownLocation(Location lastKnownLocation) {
         this.lastKnownLocation = lastKnownLocation;
     }
 
+    //Log Functions
     public String getLogDetails() {
         return String.format(
                 "Inquirer: %s, Missing Person: %s, Date of Inquiry: %s, Info Provided: %s, Last Known Location: %s",
                 inquirer.getFirstName(), missingPerson.getFirstName(), dateOfInquiry, infoProvided,
                 lastKnownLocation.getName());
     }
-
-    private boolean isValidDateFormat(String date) {
-        return date.matches("\\d{4}-\\d{2}-\\d{2}");
-    }
+    
 }
